2d779a062359db92c6a9e85df0cea25493597b11
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2025-08-09 01:50:52 +0200

    episode end, associations

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
-
-const db = require("./models");
-const postRouter = require("./routes/Posts");
+const cors = require("cors");
 
 app.use(express.json());
-app.use(require("cors")()); // ako koristiš CORS
+app.use(cors());
+
+const db = require("./models");
+
+// Routers
+const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("server is running on port 3001");
   });
 });
-
